a0612523f7a6b6fc91703de8f2322026adbb0506
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-21 21:10:39 -0300

    trabalhando com callbacks

--- index.js
+++ index.js
@@ -14,26 +14,43 @@ function getUser(callback) {
   }, 1000);
 }
 
-function getPhoneNumber(userId) {
+function getPhoneNumber(userId, callback) {
   setTimeout(() => {
-    return {
+    return callback(null , {
       phone: '1199899',
       ddd: 11
-    }
+    })
   }, 2000);
 }
 
-function getAddress(userId) {
-
+function getAddress(userId, callback) {
+  setTimeout(() => {
+    return callback(null, {
+      rua: 'dos bobos',
+      numero: 0
+    })
+  }, 2000)
 }
 
 // Callback
-function handleUser(error, user) {
+/* function handleUser(error, user) {
   console.log(`Usuario ${user}`);
 }
-
-getUser(handleUser);
-
-// const phone = getPhoneNumber(user.id);
-
-// console.log(`Telefone ${phone}`);
+ */
+getUser(handleUser = (userError, user) => {
+  // null || "" || 0 === false
+  if(userError){
+    throw userError;
+  }
+  getPhoneNumber(user.id, handlePhone = (phoneError, phone) => {
+    if (phoneError) {
+      console.error(phoneError);
+    }
+    getAddress(user.id, handleAddress = (addressError, address) => {
+      if (addressError) {
+        console.error(addressError);
+      }
+      console.log(`Nome: ${user.name}\nPhone Number: (${phone.ddd})${phone.phone}\nAddress: Rua ${address.rua} nº${address.numero}`);
+    })
+  });
+});
